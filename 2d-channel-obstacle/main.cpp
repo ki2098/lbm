011171fdcdacc7e702df1b4354695d2ac2c9478f
ki2098 <1336670149@qq.com>
2024-12-20 21:02:58 +0900

    adjust moment transform

--- 2d-channel-obstacle/main.cpp
+++ 2d-channel-obstacle/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <cmath>
 #include <string>
+#include <chrono>
 
 template<typename T, int N>
 struct vector_t {
@@ -59,7 +60,7 @@ copyin(Ve, Wgt, Cmk)
 
 const double L_ = 1;
 const double U_ = 1;
-const double Re = 1e5;
+const double Re = 100;
 const double Lx_ = 20*L_;
 const double Ly_ =  10*L_;
 const int Cells_per_length = 20;
@@ -68,7 +69,7 @@ const double nu_ = L_*U_/Re;
 const double dx_ = 1./Cells_per_length;
 const double dx = 1;
 const double Cx_ = dx_/dx;
-const double U = 0.1;
+const double U = 0.02;
 const double Cu_ = U_/U;
 const double Ct_ = Cx_/Cu_;
 const double dt = 1;
@@ -80,6 +81,7 @@ const double csqi = 3;
 const double tau = nu*csqi + 0.5;
 const double omega = 1/tau;
 const double Re_lattice = U*dx/nu;
+const double CFL = U_*dt_/dx_;
 
 const double T_ = 300;
 const int N_step = T_/dt_;
@@ -120,6 +122,66 @@ public:
     }
 };
 
+double9_t pdf_to_raw(const double9_t &f) {
+    return double9_t{{
+          f[0] + f[1] + f[2] + f[3] + f[4] + f[5] + f[6] + f[7] + f[8],
+        - f[0] + f[2] - f[3] + f[5] - f[6] + f[8],
+          f[0] + f[2] + f[3] + f[5] + f[6] + f[8],
+        - f[0] - f[1] - f[2] + f[6] + f[7] + f[8],
+          f[0] - f[2] - f[6] + f[8],
+        - f[0] - f[2] + f[6] + f[8],
+          f[0] + f[1] + f[2] + f[6] + f[7] + f[8],
+        - f[0] + f[2] - f[6] + f[8], 
+          f[0] + f[2] + f[6] + f[8]
+    }};
+}
+
+double9_t raw_to_pdf(const double9_t &m) {
+    return double9_t{{
+        0.25*m[4] - 0.25*m[5] - 0.25*m[7] + 0.25*m[8],
+       -0.5*m[3] + 0.5*m[5] + 0.5*m[6] - 0.5*m[8],
+       -0.25*m[4] - 0.25*m[5] + 0.25*m[7] + 0.25*m[8],
+       -0.5*m[1] + 0.5*m[2] + 0.5*m[7] - 0.5*m[8],
+       1.0*m[0] - 1.0*m[2] - 1.0*m[6] + 1.0*m[8],
+       0.5*m[1] + 0.5*m[2] - 0.5*m[7] - 0.5*m[8],
+       -0.25*m[4] + 0.25*m[5] - 0.25*m[7] + 0.25*m[8],
+       0.5*m[3] - 0.5*m[5] + 0.5*m[6] - 0.5*m[8],
+       0.25*m[4] + 0.25*m[5] + 0.25*m[7] + 0.25*m[8]
+    }};
+}
+
+double9_t raw_to_center(const double9_t &m, const double u, const double v) {
+    const double uu = u*u;
+    const double vv = v*v;
+    return double9_t{{
+        m[0], 
+        -m[0]*v + m[1], 
+        m[0]*vv - 2*m[1]*v + m[2], 
+        -m[0]*u + m[3],
+        -m[3]*v + m[4] - u*(-m[0]*v + m[1]),
+        m[3]*vv - 2*m[4]*v + m[5] - u*(m[0]*vv - 2*m[1]*v + m[2]),
+        m[0]*uu - 2*m[3]*u + m[6],
+       -m[6]*v + m[7] + uu*(-m[0]*v + m[1]) - 2*u*(-m[3]*v + m[4]),
+       m[6]*vv - 2*m[7]*v + m[8] + uu*(m[0]*vv - 2*m[1]*v + m[2]) - 2*u*(m[3]*vv - 2*m[4]*v + m[5])
+    }};
+}
+
+double9_t center_to_raw(const double9_t &k, const double u, const double v) {
+    const double uu = u*u;
+    const double vv = v*v;
+    return double9_t{{
+        k[0],
+        k[0]*v + k[1],
+        k[0]*vv + 2*k[1]*v + k[2],
+        k[0]*u + k[3],
+        k[3]*v + k[4] + u*(k[0]*v + k[1]),
+        k[3]*vv + 2*k[4]*v + k[5] + u*(k[0]*vv + 2*k[1]*v + k[2]),
+        k[0]*uu + 2*k[3]*u + k[6],
+        k[6]*v + k[7] + uu*(k[0]*v + k[1]) + 2*u*(k[3]*v + k[4]),
+        k[6]*vv + 2*k[7]*v + k[8] + uu*(k[0]*vv + 2*k[1]*v + k[2]) + 2*u*(k[3]*vv + 2*k[4]*v + k[5])
+    }};
+}
+
 void get_statistics(
     const double9_t &f,
     double &fluxx,
@@ -151,22 +213,8 @@ void compute_cumulants(
         get_statistics(fc, fluxx, fluxy, density);
         const double u = fluxx/density;
         const double v = fluxy/density;
-        double9_t kc{{}};
-        for (int fid = 0; fid < 9; fid ++) {
-            const int i = Ve[fid][0];
-            const int j = Ve[fid][1];
-            vector_t<double, 3> ca, cb;
-            ca[0] = cb[0] = 1;
-            for (int order = 1; order < 3; order ++) {
-                ca[order] = ca[order - 1]*(i - u);
-                cb[order] = cb[order - 1]*(j - v);
-            }
-            for (int kid = 0; kid < 9; kid ++) {
-                const int a = Cmk[kid][0];
-                const int b = Cmk[kid][1];
-                kc[kid] += ca[a]*cb[b]*fc[fid];
-            }
-        }
+        double9_t mc = pdf_to_raw(fc);
+        double9_t kc = raw_to_center(mc, u, v);
         double9_t &cc = c[lat];
         cc[0] = density;
         cc[1] = fluxy;
@@ -237,25 +285,8 @@ void compute_post_pdfs(
         kc[7] = cc[7];
         kc[8] = cc[8] + (2*kc[4]*kc[4] + kc[6]*kc[2])/density;
 
-        double uu = u*u, vv = v*v, uv = u*v;
-        double9_t &fc = fpost[lat];
-        const double M[9][9] = {
-            {  uv*(uv - u - v + 1)*0.25, u*(2*uv - u - 2*v + 1)*0.25, u*(u - 1)*0.25, v*(2*uv - 2*u - v + 1)*0.25, uv - u*0.5 - v*0.5 + 0.25, u*0.5 - 0.25, v*(v - 1)*0.25, v*0.5 - 0.25,  0.25},
-            {u*(-u*vv + u + vv - 1)*0.5,                  uv*(1 - u),  u*(1 - u)*0.5,    -u*vv + u + vv*0.5 - 0.5,               v*(1 - 2*u),      0.5 - u,   0.5 - vv*0.5,           -v,  -0.5},
-            {  uv*(uv + u - v - 1)*0.25, u*(2*uv + u - 2*v - 1)*0.25, u*(u - 1)*0.25, v*(2*uv + 2*u - v - 1)*0.25, uv + u*0.5 - v*0.5 - 0.25, u*0.5 - 0.25, v*(v + 1)*0.25, v*0.5 + 0.25,  0.25},
-            {v*(-uu*v + uu + v - 1)*0.5,    -uu*v + uu*0.5 + v - 0.5,   0.5 - uu*0.5,                  uv*(1 - v),               u*(1 - 2*v),           -u,  v*(1 - v)*0.5,      0.5 - v,  -0.5},
-            {       uu*vv - uu - vv + 1,                2*v*(uu - 1),         uu - 1,                2*u*(vv - 1),                      4*uv,          2*u,         vv - 1,          2*v,    1.},
-            {v*(-uu*v - uu + v + 1)*0.5,    -uu*v - uu*0.5 + v + 0.5,   0.5 - uu*0.5,                 uv*(-v - 1),              u*(-2*v - 1),           -u, v*(-v - 1)*0.5,     -v - 0.5,  -0.5},
-            {  uv*(uv - u + v - 1)*0.25, u*(2*uv - u + 2*v - 1)*0.25, u*(u + 1)*0.25, v*(2*uv - 2*u + v - 1)*0.25, uv - u*0.5 + v*0.5 - 0.25, u*0.5 + 0.25, v*(v - 1)*0.25, v*0.5 - 0.25,  0.25},
-            {u*(-u*vv + u - vv + 1)*0.5,                 uv*(-u - 1), u*(-u - 1)*0.5,    -u*vv + u - vv*0.5 + 0.5,              v*(-2*u - 1),     -u - 0.5,   0.5 - vv*0.5,           -v,  -0.5},
-            {  uv*(uv + u + v + 1)*0.25, u*(2*uv + u + 2*v + 1)*0.25, u*(u + 1)*0.25, v*(2*uv + 2*u + v + 1)*0.25, uv + u*0.5 + v*0.5 + 0.25, u*0.5 + 0.25, v*(v + 1)*0.25, v*0.5 + 0.25,  0.25}
-        };
-        for (int fid = 0; fid < 9; fid ++) {
-            fc[fid] = 0;
-            for (int kid = 0; kid < 9; kid ++) {
-                fc[fid] += M[fid][kid]*kc[kid];
-            }
-        }
+        double9_t mc = center_to_raw(kc, u, v);
+        fpost[lat] = raw_to_pdf(mc);
     }
 }
 
@@ -271,8 +302,8 @@ void advect(
     for (int i = 1; i < imax - 1; i ++) {
     for (int j = 1; j < jmax - 1; j ++) {
     for (int q = 0; q < 9; q ++) {
-        int dst = (i + Ve[q][0])*jmax + (j + Ve[q][1]);
-        int src = i*jmax + j;
+        int src = (i - Ve[q][0])*jmax + (j - Ve[q][1]);
+        int dst = i*jmax + j;
         f[dst][q] = fpost[src][q];
     }}}
 }
@@ -399,6 +430,16 @@ void apply_fbc(
             f[lat][fid] = fpost[lat][8 - fid];
         }
     }
+
+    #pragma acc serial \
+    present(f[:imax*jmax], fpost[:imax*jmax]) \
+    firstprivate(imax, jmax)
+    {
+        f[(ia-1)*jmax + (ja-1)][0] = fpost[(ia-1)*jmax + (ja-1)][8];
+        f[(ia-1)*jmax + (jb  )][2] = fpost[(ia-1)*jmax + (jb  )][6];
+        f[(ib  )*jmax + (ja-1)][6] = fpost[(ib  )*jmax + (ja-1)][2];
+        f[(ia  )*jmax + (jb  )][8] = fpost[(ia  )*jmax + (jb  )][0];
+    }
 }
 
 Cumu *init(int imax, int jmax, double tau) {
@@ -409,7 +450,7 @@ Cumu *init(int imax, int jmax, double tau) {
     present(cumu[0:1], cumu->c[0:imax*jmax]) \
     firstprivate(imax, jmax)
     for (int lat = 0; lat < imax*jmax; lat ++) {
-        cumu->c[lat] = get_equilibrium(1, U, 0);
+        cumu->c[lat] = get_equilibrium(1, 0, 0);
     }
 
     compute_post_pdfs(cumu->c, cumu->f, cumu->imax, cumu->jmax);
@@ -480,12 +521,15 @@ void output(Cumu *cumu, int n) {
 
 int main() {
     Cumu *cumu = init(Lx_*Cells_per_length + 2*Ghost_cell, Ly_*Cells_per_length + 2*Ghost_cell, tau);
+    auto begin = std::chrono::high_resolution_clock::now();
     for (int step = 1; step <= N_step; step ++) {
         main_loop(cumu);
         printf("\r%d/%d", step, N_step);
         fflush(stdout);
     }
-    printf("\n");
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapse = std::chrono::duration_cast<std::chrono::seconds>(end - begin);
+    printf("\n%d\n", elapse);
     output(cumu, 0);
     for (int step = 1; step <= int(100/dt_); step ++) {
         main_loop(cumu);
@@ -496,6 +540,5 @@ int main() {
         }
     }
     printf("\n");
-    // output(cumu);
     finalize(cumu);
 }
